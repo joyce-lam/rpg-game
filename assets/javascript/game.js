fe753dc8bc5f7d2faed1433e511a194f6a9343f5
Joyce <joycelamsaungan@gmail.com>
2018-01-24 20:54:32 -0800

    modifying index.html and defining characters in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+//Define character's attributes
+var harryPotter = {
+	healthPoints: 200,
+	attackPower: 20,
+	counterAttackPower: 30
+}
+
+var voldemort = {
+	healthPoints: 500,
+	attackPower: 50,
+	counterAttackPower: 50
+}
+
+var malfroy = {
+	healthPoints: 250,
+	attackPower: 25,
+	counterAttackPower: 35
+}
+
+var dementor = {
+	healthPoints: 150,
+	attackPower: 15,
+	counterAttackPower: 20
+}
+
+
+
+
